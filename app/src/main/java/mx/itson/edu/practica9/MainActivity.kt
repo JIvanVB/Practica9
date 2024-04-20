987fd0c9982205072d27769e162e93552b374b73
JIVB <darksamus0997@hotmail.com>
2024-04-20 01:04:08 -0700

    completado y funcionando, excepto por la lista

--- app/src/main/java/mx/itson/edu/practica9/MainActivity.kt
+++ app/src/main/java/mx/itson/edu/practica9/MainActivity.kt
@@ -11,38 +11,40 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
-    private val userRef = FirebaseDatabase.getInstance().getReference("Users")
+    private val userRef = FirebaseDatabase.getInstance().getReference("Users").apply {
+        addChildEventListener(object : ChildEventListener {
+                            override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
+                                val value = snapshot.value
+                                if (value !is String) return
+                                if (value !is User) return
+                                writeMark(value)
+
+                                }
+                            override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {}
+                            override fun onChildRemoved(snapshot: DataSnapshot) {}
+                            override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {}
+                            override fun onCancelled(error: DatabaseError) { }
+                            })
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         findViewById<Button>(R.id.btn_save).apply { setOnClickListener { saveMarkFromForm() } }
 
-        userRef.addChildEventListener(object : ChildEventListener {
-            override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
-                writeMark(snapshot.getValue(User::class.java))
-            }
-            override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {}
-            override fun onChildRemoved(snapshot: DataSnapshot) {}
-            override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {}
-            override fun onCancelled(error: DatabaseError) { }
-
-        })
-
     }
 
-    private fun writeMark(it: User?) {
-        findViewById<TextView>(R.id.tv_lista).apply { append("$text\n") }
-        //val listV = findViewById<TextView>(R.id.tv_lista)
-        //val text = listV.text.toString() + it.toString() + "\n"
-        //listV.text=text
+    private fun writeMark(mark: User) {
+        val listV:TextView = findViewById<TextView>(R.id.tv_lista) as TextView
+        val text = listV.text.toString() + mark.toString() + "\n"
+        listV.text=text
     }
 
     private fun saveMarkFromForm() {
         userRef.push().setValue(User(
-            (findViewById<EditText>(R.id.et_nombre)).toString(),
-            (findViewById<EditText>(R.id.et_apellido)).toString(),
-            (findViewById<EditText>(R.id.et_edad)).toString()
+            (findViewById<EditText>(R.id.et_nombre)).text.toString(),
+            (findViewById<EditText>(R.id.et_apellido)).text.toString(),
+            (findViewById<EditText>(R.id.et_edad)).text.toString()
         ))
     }
 }
